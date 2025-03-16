cd7b0e63601aa9e13a9f13320275f953f1fa3823
Tavolordk <oleaoctavio23@gmail.com>
2025-03-16 01:29:22 -0600

    Importada funcionalidad de Js de bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 // app/layout.tsx
-import './globals.css'
-import { Orbitron, Poppins } from 'next/font/google'
-import type { Metadata } from 'next'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './globals.css';
+import { Orbitron, Poppins } from 'next/font/google';
+import type { Metadata } from 'next';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const metadata: Metadata = {
   title: 'NASA Space Apps Challenge 2025 | Atoyac de Álvarez',
