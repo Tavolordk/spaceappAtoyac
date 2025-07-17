b0d2f6a7f5baf17b38b7d83acc2aec4559e5bbdb
Tavolordk <oleaoctavio23@gmail.com>
2025-07-16 22:11:49 -0600

    Nuevo diseño de pagina traducida y corregidos estilos

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -1,32 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUsers,
+  faLightbulb,
+  faTrophy,
+  faRocket,
+  faNetworkWired,
+  faChalkboardTeacher
+} from "@fortawesome/free-solid-svg-icons";
+
 const FeaturesSection = () => {
   const features = [
     {
-      icon: "fas fa-users",
+      icon: faUsers,
       title: "Colaboración en Equipo",
       description: "Trabaja con personas de diferentes perfiles para resolver desafíos globales."
     },
     {
-      icon: "fas fa-lightbulb",
+      icon: faLightbulb,
       title: "Innovación",
       description: "Desarrolla soluciones creativas con datos abiertos de la NASA y tecnologías de vanguardia."
     },
     {
-      icon: "fas fa-trophy",
+      icon: faTrophy,
       title: "Premios",
       description: "Compite por premios y reconocimiento de la NASA y agencias espaciales internacionales."
     },
     {
-      icon: "fas fa-rocket",
+      icon: faRocket,
       title: "Exploración Espacial",
       description: "Contribuye a misiones espaciales reales y proyectos científicos sobre la Tierra."
     },
     {
-      icon: "fas fa-network-wired",
+      icon: faNetworkWired,
       title: "Conexiones Profesionales",
       description: "Conecta con profesionales, científicos y entusiastas del espacio."
     },
     {
-      icon: "fas fa-chalkboard-teacher",
+      icon: faChalkboardTeacher,
       title: "Aprendizaje",
       description: "Adquiere nuevas habilidades y conocimientos mediante talleres y mentorías."
     }
@@ -39,11 +49,11 @@ const FeaturesSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {features.map((feature, index) => (
             <div key={index} className="bg-white p-6 rounded-lg shadow-md border border-[#EDE0D4]">
-              <div className="text-4xl text-[#D4471D] mb-4">
-                <i className={feature.icon}></i>
+              <div className="text-4xl text-[#D4471D] mb-4 flex justify-center">
+                <FontAwesomeIcon icon={feature.icon} className="text-[#D4471D] text-4xl mb-4" />
               </div>
-              <h3 className="text-xl font-semibold text-[#1D3557] mb-2">{feature.title}</h3>
-              <p className="text-[#264653]">{feature.description}</p>
+              <h3 className="text-xl font-semibold text-[#1D3557] mb-2 text-center">{feature.title}</h3>
+              <p className="text-[#264653] tex-justify">{feature.description}</p>
             </div>
           ))}
         </div>
