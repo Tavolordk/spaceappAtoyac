436881b1290a37bbd3f3ca2a475d613b08cf0d99
Tavolordk <oleaoctavio23@gmail.com>
2025-07-18 16:57:58 -0600

    cambiado captcha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ import AboutRegisterSection from '@/components/AboutRegisterSection';
 import FeaturesSection from '@/components/FeaturesSection';
 import SponsorsSection from '@/components/SponsorsSection';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 export default function HomePage() {
   return (
     <>
+    <Script
+  src="https://www.google.com/recaptcha/api.js?render=6Lda_IcrAAAAAMsdSY6DfMXEwH5eTD9nzn_OM6EP"
+  strategy="afterInteractive"
+/>
       <Navbar />
       <Hero />
       <AboutRegisterSection />
