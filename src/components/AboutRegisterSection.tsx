ee506d063336763587ed1c80175b64acc7445d26
Tavolordk <oleaoctavio23@gmail.com>
2025-07-18 16:23:15 -0600

    sitio web con captcha

--- src/components/AboutRegisterSection.tsx
+++ src/components/AboutRegisterSection.tsx
@@ -81,6 +81,7 @@ const AboutRegisterSection = () => {
                 <option value="cbtis23">CBTIS 23</option>
                 <option value="otro">Otro</option>
               </select>
+              <div className="g-recaptcha" data-sitekey="6Lda_IcrAAAAAMsdSY6DfMXEwH5eTD9nzn_OM6EP"></div>
               <button type="submit" className="w-full bg-[#2A9D8F] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#21867B] transition duration-300">
                 Enviar Registro
               </button>
